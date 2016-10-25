0aba6cec41f496054ba94b888a0d9bc08fdfce41
Gajendra kumar <gk797532@gmail.com>
2016-10-25 19:31:33 +0530

    Add files via upload

---
+++ gen/com/example/storeimageexternalstorage_android_examples/com/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.storeimageexternalstorage_android_examples.com;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
